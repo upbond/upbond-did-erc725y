5e7cb65f8574f3960834822f9f78b1865c605f38
codev911 <codev911@gmail.com>
2022-12-14 13:06:07 +0700

    add ERC725XCore

--- contracts/UpbondDID.sol
+++ contracts/UpbondDID.sol
@@ -1,3 +1,4 @@
+import "./ERC725/ERC725XCore.sol";
 import "./ERC725/ERC725YCore.sol";
 import "./abstract/str2bytes.sol";
 
@@ -7,9 +8,22 @@ pragma solidity ^0.8.0;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract UpbondDID is ERC725YCore, str2bytes {
+contract UpbondDID is ERC725XCore, ERC725YCore, str2bytes {
     constructor(address newOwner) {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         OwnableUnset._setOwner(newOwner);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC725XCore, ERC725YCore)
+        returns (bool)
+    {
+        return
+            interfaceId == _INTERFACEID_ERC725X ||
+            interfaceId == _INTERFACEID_ERC725Y ||
+            super.supportsInterface(interfaceId);
     }
 }
