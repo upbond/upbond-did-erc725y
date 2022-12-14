910168da92b2183c5c237d9e605d64b43b547a6e
codev911 <codev911@gmail.com>
2022-12-14 15:14:24 +0700

    create deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const UpbondDID = await hre.ethers.getContractFactory("UpbondDID");
+  const upbondDID = await UpbondDID.deploy();
+  await upbondDID.deployed();
+  console.log(`Upbond DID implementation deployed at : ${upbondDID.address}`);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const UpbondDIDFactory = await hre.ethers.getContractFactory("UpbondDIDFactory");
+  const upbondDIDFactory = await UpbondDIDFactory.deploy(upbondDID.address);
+  await upbondDIDFactory.deployed();
+  console.log(`Upbond DID factory deployed at : ${upbondDIDFactory.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
