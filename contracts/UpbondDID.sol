1b26fbdd297b07459aa055dd28376da7f795f1ee
codev911 <codev911@gmail.com>
2022-12-14 13:01:32 +0700

    add str2bytes abstract contract for convert data

--- contracts/UpbondDID.sol
+++ contracts/UpbondDID.sol
@@ -1,4 +1,5 @@
 import "./ERC725/ERC725YCore.sol";
+import "./abstract/str2bytes.sol";
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
@@ -6,7 +7,7 @@ pragma solidity ^0.8.0;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract UpbondDID is ERC725YCore {
+contract UpbondDID is ERC725YCore, str2bytes {
     constructor(address newOwner) {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         OwnableUnset._setOwner(newOwner);
---
+++ contracts/abstract/str2bytes.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+abstract contract str2bytes {
+    function toBytes(string memory _string) public pure returns(bytes memory) {
+        return bytes(_string);
+    }
+
+    function toBytes32(string memory _string) public pure returns(bytes32) {
+        bytes32 res;
+
+        assembly {
+            res := mload(add(_string, 32))
+        }
+
+        return res;
+    }
+
+    function fromBytes(bytes memory _bytes) public pure returns(string memory) {
+        return string(_bytes);
+    }
+
+    function fromBytes32(bytes32 _bytes32) public pure returns(string memory) {
+        bytes memory res = new bytes(32);
+
+        for (uint256 i; i < 32; i++) {
+            res[i] = _bytes32[i];
+        }
+
+        return string(res);
+    }
+}
